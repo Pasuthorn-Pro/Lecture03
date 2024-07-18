9264204e16bf83b3ea6701f93cb60926d57f4619
Pasuuu-yahoo <pasuthorn1406@gmail.com>
2024-07-18 16:44:58 +0700

    Lecture03

--- Calculate_Average_Score.py
+++ Calculate_Average_Score.py
@@ -5,5 +5,5 @@ third_score_test = int(input("Enter third test score: "))
 average = ((first_score_test + second_score_test + third_score_test)/3)
 print("average is ", format(average,".2f"))
 
-if average >= 95 :
+if average > 95 :
     print("Congratulations\nThat's a great average!")
